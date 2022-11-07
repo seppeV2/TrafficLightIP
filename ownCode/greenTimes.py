a7de81defc78c3c55d4f4f06abf345ce0e5001be
seppeV2 <seppe.vilain@student.kuleuven.be>
2022-11-07 12:46:22 +0100

    P0 policy added
    there is still a bug in the normal algortihm as it does weird things + we need to find a solution for the P0 when u is smaller than 0

--- ownCode/greenTimes.py
+++ ownCode/greenTimes.py
@@ -38,12 +38,28 @@ def websterGreenTimes(caps, flows):
     #add the zeros where the flow is zero
     for i in range(len(zero_flow)):
         g_times.insert(zero_flow[i],0.01)
+    
+    #check for not to small green times
+    
     print('greens times in webster loop= '+str(g_times))
     return g_times
 
 
-def P0policyGreenTimes():
-    return
+def P0policyGreenTimes(caps, flows):
+    #solving a linear system of equations 
+            #for al links per intersection xi/(si*gi) needs to be equal to 
+            #sum of g's needs to be 1
+    left_side = []
+    right_side = []
+    last_term = [1]
+    for i in range(len(caps)-1):
+        left_side.append([1,-1])
+        right_side.append((flows[i]/caps[i])-(flows[i+1]/caps[i+1]))
+        last_term.append(1)
+    left_side.append(last_term)
+    right_side.append((1))
+    g_times = np.linalg.solve(left_side,right_side)
+    return g_times
 
 def otherPolicy():
     return
--- ownCode/main.py
+++ ownCode/main.py
@@ -43,20 +43,22 @@ def main():
 
     #starting with 0.5 at every two link node
         #hardCoded for simple
-    firstGreen = {0: 1, 1: 0.5, 2: 1, 3: 0.5, 4: 1}
+    firstGreen = {0: 0.5, 1: 1, 2: 1, 3: 0.5}
         #hardCoded for complex 
     #firstGreen = {0: 1, 1: 1, 2: 0.5, 3: 1, 4: 1, 5: 1, 6: 1, 7: 0.5, 8: 0.5, 9: 1, 10: 1, 11: 1, 12: 0.5, 13: 0.5, 14: 1, 15: 0.5}
 
 
     #initial msa without traffic lights
-    result2 = assignment.run('msa')
+            #result2 = assignment.run('msa')
     result = assignment.run_greens('msa', firstGreen)
     #calculate the first green times according the first static assignment
     print('flows: '+str(result.flows))
-    greens = get_green_times(assignment.internal_network.links.capacity,result.flows,assignment.internal_network)
-    print('greens: '+ str(greens))
+    greens = get_green_times(assignment.internal_network.links.capacity,result.flows,assignment.internal_network, 'webster')
+    print('greensWebster: '+ str(greens))
+    greens2 = get_green_times(assignment.internal_network.links.capacity,result.flows,assignment.internal_network, 'P0')
+    print('greensP0: '+ str(greens))
     show_network(g, flows = result.flows, euclidean=True)
-    show_network(g, flows = result2.flows, euclidean = True)
+            #show_network(g, flows = result2.flows, euclidean = True)
     #start the loop
     print('START THE LOOP')
         #initialise parameters and variables
