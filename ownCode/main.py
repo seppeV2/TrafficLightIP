94578aedb1cf3666b3c379cbd332f8f497402be5
seppeV2 <seppe.vilain@student.kuleuven.be>
2022-11-24 11:57:27 +0100

    added the functions to auto generate Summary files

--- ownCode/main.py
+++ ownCode/main.py
@@ -5,10 +5,14 @@ import matplotlib.pyplot as plt
 
 
 from dyntapy import show_network
-
+from network_summary import create_summary
 from ownFunctions import makeOwnToyNetwork, getODGraph
 from dyntapy_green_time_change import GreenStaticAssignment
 from greenTimes import get_green_times
+from bokeh.embed import file_html
+from bokeh.resources import CDN
+from bokeh.io import export_png
+
 
 
 
@@ -17,7 +21,7 @@ def main():
         #two cost functions at the moment
         # 'bpr' to use the bpr cost function
         # 'WebsterTwoTerm' to use the webster two term delay cost function
-    methodCost = 'bpr'
+    methodCost = 'WebsterTwoTerm'
 
         #two green time policies
         # 'equisaturation' 
@@ -29,11 +33,14 @@ def main():
     #setup
     print("STARTING SETUP")
     g, ODcentroids, odFile = makeOwnToyNetwork('simple')
-    ODMatrix = str(pathlib.Path(__file__).parent)+'/data/'+str(odFile)
-    odGraph = getODGraph(ODMatrix, ODcentroids)
+    ODMatrix_location = str(pathlib.Path(__file__).parent)+'/data/'+str(odFile)
+    ODMatrix = np.genfromtxt(ODMatrix_location, delimiter=',')
+    odGraph = getODGraph(ODMatrix_location, ODcentroids)
+    
+    demand = ODMatrix[np.nonzero(ODMatrix)][0]
     assignment = GreenStaticAssignment(g, odGraph)
 
-   
+    summary_string = 'Cost method: {}\nPolicy: {}\nDemand: {}\n\n'.format(methodCost, methodGreen,demand)
 
     print("RUNNING FIRST STATIC ASSIGNMENT WITH TRAFFIC LIGHTS (equal distributed)")
 
@@ -43,7 +50,7 @@ def main():
 
         #hardCoded for complex 
     #firstGreen = {0: 1, 1: 1, 2: 0.5, 3: 1, 4: 1, 5: 1, 6: 1, 7: 0.5, 8: 0.5, 9: 1, 10: 1, 11: 1, 12: 0.5, 13: 0.5, 14: 1, 15: 0.5}
-
+    summary_string += "initial greens: link 0 = {},   link 3 = {}\n".format(firstGreen[0], firstGreen[3])
 
     #initial msa without traffic lights
             #result2 = assignment.run('msa')
@@ -51,9 +58,10 @@ def main():
     #calculate the first green times according the first static assignment
     print('\nflows: '+str(result.flows))
     print('free flow cost: {}'.format(ff_tt))
-
+    summary_string += "free flow cost: link 0 = {},   link 3 = {}\n\n".format(round(ff_tt[0],3), round(ff_tt[3],3))
     greens,diff = get_green_times(assignment.internal_network.links.capacity,result.flows,assignment, methodGreen, firstGreen, g, ff_tt)
     print('greens: '+ str(greens))
+
 
 
     #show_network(g, flows = result.flows, euclidean=True)
@@ -76,7 +84,7 @@ def main():
     prev_flow = np.zeros(len(result.flows))
     while gap > delta and safety < maxLoops:
         safety += 1
-        print('####\tLOOP = '+str(safety))
+        print('#### LOOP = '+str(safety))
 
         newResult, ff_tt, dos = assignment.run_greens('msa', greens,methodCost,g)
         print('flows: {}'.format(newResult.flows))
@@ -105,8 +113,14 @@ def main():
         result = newResult
         greens = newGreens
 
-    show_network(g, flows = result.flows, euclidean=True)
+    summary_string += "link flows: link 0 = {},   link 3 = {}\n".format(round(result.flows[0],3), round(result.flows[3],3))
+    summary_string += "link costs: link 0 = {},   link 3 = {}\n".format(round(result.link_costs[0],3), round(result.link_costs[3],3))
+    summary_string += "final green times: link 0 = {},   link 3 = {}\n".format(round(greens[0],3), round(greens[3],3))
+    
 
+    graph = show_network(g, flows = result.flows, euclidean=True,return_plot=True)
+    export_png(graph, filename=str(pathlib.Path(__file__).parent)+'/summaryFiles/rawFigures/network.png' )
+    listOfPlots = ['network.png']
     if plot:
         ## graph plots 
         """ plt.figure()
@@ -118,12 +132,17 @@ def main():
         plt.plot(cost_link_b)
         plt.title('Evolution of the link costs on the two intersection links')
         plt.legend(['link 0','link 3'])
+        plt.savefig(str(pathlib.Path(__file__).parent)+'/summaryFiles/rawFigures/linkCost.png')
+        listOfPlots.append('linkCost.png')
 
         plt.figure()
         plt.plot(green_link_a)
         plt.plot(green_link_b)
         plt.title('Green time evolution')
         plt.legend(['link 0', 'link 3'])
+        plt.savefig(str(pathlib.Path(__file__).parent)+'/summaryFiles/rawFigures/greenTime.png')
+        listOfPlots.append('greenTime.png')
+
         """ plt.figure()
         plt.plot(diff_l)
         plt.title("evolution of policy constraint (gap = change between the two)") """
@@ -134,9 +153,12 @@ def main():
             plt.plot(dos_link_b)
             plt.title("Dos evolution through the msa steps")
             plt.legend(['link 0', 'link 3'])
+            plt.savefig(str(pathlib.Path(__file__).parent)+'/summaryFiles/rawFigures/dos.png')
+            listOfPlots.append('dos.png')
 
+    print(summary_string)
+    create_summary(listOfPlots, summary_string, methodCost, methodGreen, demand )
 
-        plt.show()
 
 
 
---
+++ ownCode/network_summary.py
@@ -0,0 +1,38 @@
+import pathlib
+from PIL import Image, ImageDraw, ImageFont
+import numpy as np
+from html2image import Html2Image
+#install opencv-python for this (via pip)
+import cv2
+
+def text_to_image(textString):
+    img = Image.new('RGB', (500, 400), (255, 255, 255))
+    d = ImageDraw.Draw(img)
+    font = ImageFont.truetype(str(pathlib.Path(__file__).parent)+"/data/Gidole-Regular.ttf", size=17)
+    d.text((20, 20), textString, fill=(0, 0, 0), font = font)
+    file_name = 'text.png'
+    img.save(str(pathlib.Path(__file__).parent)+'/summaryFiles/rawFigures/'+file_name)
+    return file_name
+
+def create_summary(listOfPlots, string, method, policy, demand):
+    text_image = text_to_image(string)
+    listOfPlots.insert(1,text_image)
+    vstack = []
+    hstack = []
+    for i in range(len(listOfPlots)):
+        figure = cv2.imread(str(pathlib.Path(__file__).parent)+'/summaryFiles/rawFigures/'+listOfPlots[i])
+        figure = cv2.resize(figure, (500,400))
+        hstack.append(figure)
+        if (i%2 == 1 and i>0):
+            vstack.append(np.hstack(hstack))
+            hstack = []
+        
+        if i == len(listOfPlots)-1 and i%2 == 0:
+            hstack.append(np.full((400,500,3),255, np.uint8))
+            vstack.append(np.hstack(hstack))      
+    vstack = np.vstack(vstack)
+
+    cv2.imwrite(str(pathlib.Path(__file__).parent)+'/summaryFiles/summary_{}_{}_D={}.png'.format(method, policy, demand), vstack)
+
+
+
