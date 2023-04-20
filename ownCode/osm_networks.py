046a1c27e380145a397fe359dbfd93ac138c1a35
seppeV2 <seppe.vilain@student.kuleuven.be>
2023-04-20 22:54:01 +0200

    osm start

--- ownCode/osm_networks.py
+++ ownCode/osm_networks.py
@@ -9,25 +9,24 @@ import numpy as np
 def make_osm_network(place: str, distance_outer: int, distance_inner:int):
 
     g = road_network_from_place(place, buffer_dist_close= distance_inner, buffer_dist_extended=distance_outer)
-    X,Y, name, place = auto_configured_centroids(place, buffer_dist_close = distance_inner, buffer_dist_extended=distance_outer)
-    res = generate_od_xy(30,'Leuven',2000)
-    """
+    X,Y, names, places = auto_configured_centroids(place, buffer_dist_close = distance_inner, buffer_dist_extended=distance_outer)
+    res = generate_od_xy(100,place,1000)
+    
     auto_centroid = {'X':[], 'Y':[],'name':[], 'place':[]}
-    for x,y,_name, _place in zip(X, Y, name, place):
+    for x,y,_place,_name in zip(X, Y, places, names):
         if _place != '-':
             auto_centroid['X'].append(x)
             auto_centroid['Y'].append(y)
-            auto_centroid['name'].append(_name)
-            auto_centroid['place'].append(_place)
     g = add_centroids(g, np.array(auto_centroid['X']),np.array(auto_centroid['Y']),k = 3, method = 'link')
-    """
-    g = add_centroids(g, X, Y, k=2, method = 'link')
+    
+    #g = add_centroids(g, X, Y, k=2, method = 'link')
     g = relabel_graph(g)
     od_graph = parse_demand(res, g)
     
 
-    assignment = StaticAssignment(g, od_graph)
-    result = assignment.run('msa')
-    show_network(g,flows=result.flows)
+    #assignment = StaticAssignment(g, od_graph)
+    #result = assignment.run('msa')
+    #show_network(g, flows = result.flows)
 
-make_osm_network('Leuven', 2000,2500)
+    return g, od_graph
+make_osm_network('Veurne', 0,5000)
